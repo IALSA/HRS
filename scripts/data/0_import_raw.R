f9b4d54a442f6c1698339212db26e4ce5c41b5a6
Andrey Koval <koval.andrey@gmail.com>
2015-09-22 16:19:28 -0700

    #1 names and labels

--- scripts/data/0_import_raw.R
+++ scripts/data/0_import_raw.R
@@ -20,7 +20,7 @@ fileNames <- basename(filePaths) # save only the last component
   #fileName <- tail(strsplit(filePath, "/|.sav")[[1]], n=1)
   #oneFile <- Hmisc::spss.get(filePath, use.value.labels = TRUE)
   #saveRDS(oneFile, paste0("./data/derived/unshared/", fileName, ".rds")) # all raw data
-}
+# }
 
 #collect all RDS in a list object
 pathFilesRDS <- pathFiles <- file.path(pathDir,"data/derived/unshared//")
@@ -28,6 +28,55 @@ filePathsRDS <- list.files(pathFiles, full.names=T, recursive=T, pattern="rds$")
 fileNamesRDSbase <- basename(filePathsRDS) # save only the last component
 fileNamesRDS <- gsub(".rds","",fileNamesRDSbase) # remove extention from the filename
 lsRAND <- list() # create empty list to population
+
+psychosocial <- c("h04f1a","h06f2b","h08f2a","h10f4a","h12e1a")
+#### Development ####
+ds04 <- readRDS("./data/derived/unshared/RAND/h04f1a.rds")
+ds06 <- readRDS("./data/derived/unshared/RAND/h06f2b.rds")
+ds08 <- readRDS("./data/derived/unshared/RAND/h08f2a.rds")
+ds10 <- readRDS("./data/derived/unshared/RAND/h10f4a.rds")
+ds12 <- readRDS("./data/derived/unshared/RAND/h12e1a.rds")
+
+# str(ds04)
+# dim(ds04)
+# 
+# (ds <- ds04[,c("HHIDPN","JA028")])
+# str(ds)
+# head(ds)
+
+## @knitr define_lookup_function
+names_labels <- function(ds0){
+    nl <- data.frame(matrix(NA, nrow=ncol(ds0), ncol=2))
+    names(nl) <- c("name","label")
+      for (i in seq_along(names(ds0))){
+        # i = 2
+        nl[i,"name"] <- attr(ds0[i], "names")
+          if(is.null(attr(ds0[[i]], "label")) ){
+          nl[i,"label"] <- NA}else{
+          nl[i,"label"] <- attr(ds0[,i], "label")  
+          }
+      }
+    return(nl)
+}
+
+
+nl04 <- names_labels(ds0=ds04)
+nl06 <- names_labels(ds0=ds06)
+nl08 <- names_labels(ds0=ds08)
+nl10 <- names_labels(ds0=ds10)
+nl12 <- names_labels(ds0=ds12)
+
+
+write.csv(nl04,"./data/derived/items/nl04.csv")
+write.csv(nl06,"./data/derived/items/nl06.csv")
+write.csv(nl08,"./data/derived/items/nl08.csv")
+write.csv(nl10,"./data/derived/items/nl10.csv")
+write.csv(nl12,"./data/derived/items/nl12.csv")
+
+
+#### Developmental script beyond this point down ####
+
+#1. Complete list of variables that have monolythic names ("gender")
 
 #for(i in 1:length(filePathsRDS)){
  # lsRAND[[i]] <- readRDS(filePathsRDS[i])
@@ -47,69 +96,12 @@ for(i in 1:length(psychosocial)){
   lsPsychosocial[[i]]<- readRDS(paste0(pathFilesRDS, psychosocial[i],".rds"))
 }
 
-
-#### Developmental script beyond this point down ####
-
-## @knitr names_labels
-names_labels <- function(file){
-  if(is.null(file)){cat("You don't have this file")}else{
-    ds0 <- file
-    nl <- data.frame(matrix(NA, nrow=ncol(ds0), ncol=2))
-    names(nl) <- c("name","label")
-      for (i in seq_along(names(ds0))){
-        nl[i,"name"] <- attr(ds0[i], "names")
-          if(is.null(attr(ds0[[i]], "label")) ){
-          nl[i,"label"] <- NA}else{
-          nl[i,"label"] <- attr(ds0[[i]], "label")  
-          }
-      }
-    return(nl)
-  }
-}
-
-## @knitr export_names_labels
-explort_names_labels <- function(file){
-  ds0 <- file
-  nl <- data.frame(matrix(NA, nrow=ncol(ds0), ncol=2))
-  names(nl) <- c("name","label")
-  for (i in seq_along(names(ds0))){
-    nl[i,"name"] <- attr(ds0[i], "names")
-      if(is.null(attr(ds0[[i]], "label")) ){
-      nl[i,"label"] <- NA}else{
-      nl[i,"label"] <- attr(ds0[[i]], "label")  
-      }
-  }
-  write.table(nl,"./data/derived/rename/test.csv", sep=",")
-}
-explort_names_labels(ds)
-  
+files <- c("h04f1a","h06f2b","h08f2a","h10f4a","h12e1a")
+id <- rep("HHIDPN", length(files))
+sex <- c("GENDER", "GENDER", "gender", "gender", "gender")
+age <- c("JA019", "KA019", "LA019", "MA019", "NA019") # J - wave indication, A - section name
 
 
 
 
-  
-names(datas_raw) <- fileNames
-
-class(datas_raw)
-names(datas_raw)
-saveRDS(datas_raw,"./data/derived/unshared/datas_raw.rds" ) # all raw data
-datas_raw <- readRDS("./data/derived/unshared/datas_raw.rds") 
-names(datas_raw)
-filesToCombine <- c(
-  "h92f1b.sav"  
- ,"h94f1a.sav"  
- ,"h96f4a.sav"  
- ,"h98f2c.sav" 
- ,"h00f1c.sav" 
- ,"h02f2c.sav"  
- ,"h04f1a.sav"  
- ,"h06f2b.sav"  
- ,"h08f2a.sav"  
- ,"h10f4a.sav" 
-)
-
-ls_raw <- datas_raw[filesToCombine] 
-
-
-names(ls_raw[[1]])
 
