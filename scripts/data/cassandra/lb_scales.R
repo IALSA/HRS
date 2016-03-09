cd9bd97129e0988d1c8b779d13cced13efc41004
casslbrown <clb@uvic.ca>
2016-03-09 12:07:13 -0800

    update

--- scripts/data/cassandra/lb_scales.R
+++ scripts/data/cassandra/lb_scales.R
@@ -86,6 +86,7 @@ ds1$lonemean <- ifelse(ds1$lonemiss<6, ds1$lonetot/(11-ds1$lonemiss), NA)
 
 summary(ds1$lonemean)
 
+
 #Create activity variable list
 #start by renaming the activity variables for consistency
 #Rename for 2014
@@ -129,6 +130,7 @@ ds1$otherAct <- ds1$act8+ds1$act10+ds1$act16+ds1$act17+ds1$act18
 
 summary(ds1$mixAct)
 
+
 ####
 
 
@@ -137,9 +139,9 @@ summary(ds1$mixAct)
 
 
 # rename Life satisfaction '14
-setnames(ds1, old=c('lb002a','lb002b','lb002c','lb002d','lb002e'),new=('lifesat1','lifesat2','lifesat3','lifesat4','lifesat5'))
+setnames(ds1, old=c("lb002a","lb002b","lb002c","lb002d","lb002e"), new= c("lifesat1","lifesat2","lifesat3","lifesat4","lifesat5"))
   
-
+names(ds1)
 
 attr(ds1$lifesat1,"label") <- "Q02A. LIFE IS CLOSE TO IDEAL"
 attr(ds1$lifesat2,"label") <- "Q02B. CONDITIONS OF LIFE ARE EXCELLENT"
@@ -147,10 +149,10 @@ attr(ds1$lifesat3,"label") <- "Q02C. SATISFIED WITH LIFE"
 attr(ds1$lifesat4,"label") <- "Q02D. HAVE IMPORTANT THINGS IN LIFE"
 attr(ds1$lifesat5,"label") <- "Q02E. CHANGE NOTHING IF LIVED LIFE OVER"
 
-which(colnames(ds1)=="lifesat1")
-which(colnames(ds1)=="lifesat5")
+s <- which(colnames(ds1)=="lifesat1")
+f <- which(colnames(ds1)=="lifesat5")
 
-ds1$lifetot <- rowSums(ds1[428:432], na.rm=TRUE)
+ds1$lifetot <- rowSums(ds1[s:f], na.rm=TRUE)
 m1 <- ifelse(is.na(ds1$lifesat1)==TRUE, 1, 0)
 m2 <- ifelse(is.na(ds1$lifesat2)==TRUE, 1, 0)
 m3 <- ifelse(is.na(ds1$lifesat3)==TRUE, 1, 0)
@@ -158,7 +160,7 @@ m4 <- ifelse(is.na(ds1$lifesat4)==TRUE, 1, 0)
 m5 <- ifelse(is.na(ds1$lifesat5)==TRUE, 1, 0)
 ds1$lifesatmiss <- m1+m2+m3+m4+m5
 summary(ds1$lifesatmiss)
-sum(ds1)
+
 #Calculate a life satisfaction score averaging across 5 items, less if some are missing.
 #Note that '06 scale is 1-6, '08 and onwards scale is 1-7.
 ds1$lifesatm <- ifelse(ds1$lifesatmiss<3, ds1$lifetot/(5-ds1$lifesatmiss), NA)
@@ -171,13 +173,11 @@ summary(ds1$lifesatm)
 #Q15-18 friends
 
 #composition of social network '14 (note the variable numbers change)
-ds3 <- within(ds1, {snspouse <- lb003
-                    snchild <- lb006
-                    snfamily <- lb010
-                    snfriends <- lb014})
+setnames(ds1, old = c('lb003','lb006','lb010','lb014'), new=c('snspouse','snchild','snfamily','snfriends'))
+
 
 #Past here not year specific
-ds3 <- within(ds3, {snspouse[snspouse==5]<- 0
+ds1 <- within(ds1, {snspouse[snspouse==5]<- 0
                     snchild[snchild==5]<-0
                     snfamily[snfamily==5]<-0
                     snfriends[snfriends==5]<-0
@@ -185,42 +185,157 @@ ds3 <- within(ds3, {snspouse[snspouse==5]<- 0
                     })
 
 #number of close relationships, spouse excluded from total score
-ds3 <- within(ds3, {closespouse<-lb005
-                    closechild <-lb009
-                    closefam <- lb013
-                    closefri <- lb017
-                    closerel <- lb009+lb013+lb017})
-#social network contact excluding spouse
-ds3 <- within(ds3, {meetch <- lb008a
-                    speakch <- lb008b
-                    writech <- lb008c
-                    socialmech <- lb008d
-                    meetfam<- lb012a
-                    speakfam <- lb012b
-                    writefam <- lb012c
-                    socialmefam <- lb012d
-                    meetfri <- lb016a
-                    speakfri <- lb016b
-                    writefr <- lb016c
-                    socialmefri <- lb016d})
-ds3 <- within(ds3, {snfriends[snfriends==7] <- NA})
+setnames(ds1, old=c('lb005','lb009','lb013','lb017'), new=c('closespouse','closechild','closefam','closefri'))
+names(ds1)
 
-summary(ds3$snspouse)
-summary(ds3$snchild)
-summary(ds3$snfamily)
-summary(ds3$snfriends)
-which(ds3$snfriends==7)
-#recode the 7 value to NA
-ds3$snfriends[ds3$snfriends==7] <- NA
-which(colnames(ds3)=="snspouse")
-which(colnames(ds3)=="snfriends")
-ds3$socnetwork <- rowSums(ds3[436:439])
-summary(ds3$socnetwork)
+ds1 <- within(ds1,{closerel<-closechild+closefam+closefri})
+summary(ds1$closerel)
+
+#social network contact excluding spouse
+setnames(ds1, old=c("lb008a",'lb008b','lb008c','lb008d','lb012a','lb012b','lb012c','lb012d','lb016a','lb016b','lb016c','lb016d'),
+         new=c('mtchild','spkchild','wrtchild','mediachild','mtfam','spkfam','wrtfam','mediafam','mtfriend','spkfriend','wrtfriend','mediafriend'))
+
+s <- which(colnames(ds1)=="snspouse")
+f <- which(colnames(ds1)=="snfriends")
+ds1$socnetwork <- rowSums(ds1[s:f])
+summary(ds1$socnetwork)
+
+#Perceived social support(or relationship quality)
+#Positive social support rename items associated with spouse
+setnames(ds1, old=c('lb004a','lb004b','lb004c','lb004d','lb004e','lb004f','lb004g'), new=c('ssup1sp','ssup2sp','ssup3sp','ssup4sp','ssup5sp','ssup6sp','ssup7sp'))
+setnames(ds1, old=c('lb007a','lb007b','lb007c','lb007d','lb007e','lb007f','lb007g'), new=c('ssup1ch','ssup2ch','ssup3ch','ssup4ch','ssup5ch','ssup6ch','ssup7ch'))
+setnames(ds1, old=c('lb011a','lb011b','lb011c','lb011d','lb011e','lb011f','lb011g'), new=c('ssup1fam','ssup2fam','ssup3fam','ssup4fam','ssup5fam','ssup6fam','ssup7fam'))
+setnames(ds1, old=c('lb015a','lb015b','lb015c','lb015d','lb015e','lb015f','lb015g'), new=c('ssup1fr','ssup2fr','ssup3fr','ssup4fr','ssup5fr','ssup6fr','ssup7fr'))
+
+typeof(ds1$ssup1ch)
+print(ds1$ssup1sp)
+#reverse code all items
+ds1$ssup1sp <- plyr::mapvalues(ds1$ssup1sp, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup2sp <- plyr::mapvalues(ds1$ssup2sp, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup3sp <- plyr::mapvalues(ds1$ssup3sp, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup4sp <- plyr::mapvalues(ds1$ssup4sp, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup5sp <- plyr::mapvalues(ds1$ssup5sp, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup6sp <- plyr::mapvalues(ds1$ssup6sp, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup7sp <- plyr::mapvalues(ds1$ssup7sp, from=c(1,2,3,4), to =c(4,3,2,1))
+
+ds1$ssup1ch <- plyr::mapvalues(ds1$ssup1ch, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup2ch <- plyr::mapvalues(ds1$ssup2ch, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup3ch <- plyr::mapvalues(ds1$ssup3ch, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup4ch <- plyr::mapvalues(ds1$ssup4ch, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup5ch <- plyr::mapvalues(ds1$ssup5ch, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup6ch <- plyr::mapvalues(ds1$ssup6ch, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup7ch <- plyr::mapvalues(ds1$ssup7ch, from=c(1,2,3,4), to =c(4,3,2,1))
+
+ds1$ssup1fam<- plyr::mapvalues(ds1$ssup1fam, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup2fam<- plyr::mapvalues(ds1$ssup2fam, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup3fam<- plyr::mapvalues(ds1$ssup3fam, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup4fam<- plyr::mapvalues(ds1$ssup4fam, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup5fam<- plyr::mapvalues(ds1$ssup5fam, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup6fam<- plyr::mapvalues(ds1$ssup6fam, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup7fam<- plyr::mapvalues(ds1$ssup7fam, from=c(1,2,3,4), to =c(4,3,2,1))
+
+ds1$ssup1fr<- plyr::mapvalues(ds1$ssup1fr, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup2fr<- plyr::mapvalues(ds1$ssup2fr, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup3fr<- plyr::mapvalues(ds1$ssup3fr, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup4fr<- plyr::mapvalues(ds1$ssup4fr, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup5fr<- plyr::mapvalues(ds1$ssup5fr, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup6fr<- plyr::mapvalues(ds1$ssup6fr, from=c(1,2,3,4), to =c(4,3,2,1))
+ds1$ssup7fr<- plyr::mapvalues(ds1$ssup7fr, from=c(1,2,3,4), to =c(4,3,2,1))
+
+ds1$pssspouse <- ds1$ssup1sp+ds1$ssup2sp+ds1$ssup3sp
+ds1$psschild <- ds1$ssup1ch+ds1$ssup2ch+ds1$ssup3ch
+ds1$pssfam <- ds1$ssup1fam+ds1$ssup2fam+ds1$ssup3fam
+ds1$pssfr <- ds1$ssup1fr+ds1$ssup2fr+ds1$ssup3fr
+
+ds1$nssspouse <- ds1$ssup4sp+ds1$ssup5sp+ds1$ssup6sp+ds1$ssup7sp
+ds1$nsschild <- ds1$ssup4ch+ds1$ssup5ch+ds1$ssup6ch+ds1$ssup7ch
+ds1$nssfam <- ds1$ssup4fam+ds1$ssup5fam+ds1$ssup6fam+ds1$ssup7fam
+ds1$nssfr <- ds1$ssup4fr+ds1$ssup5fr+ds1$ssup6fr+ds1$ssup7ch
+
+#Positive and Negative Affect
+setnames(ds1, old=c('lb026a','lb026b','lb026c','lb026d','lb026e','lb026f','lb026g','lb026h','lb026i','lb026j','lb026k','lb026l','lb026m','lb026n'
+                    ,'lb026o','lb026p','lb026q','lb026r','lb026s','lb026t','lb026u','lb026v','lb026w','lb026x','lb026y'),
+         new=c('panas1','panas2','panas3','panas4','panas5','panas6','panas7','panas8','panas9','panas10','panas11','panas12','panas13'
+               ,'panas14','panas15','panas16','panas17','panas18','panas19','panas20','panas21','panas22','panas23','panas24','panas25'))
+print(ds1$panas1)
+s <- which(colnames(ds1)=="panas1")
+f <- which(colnames(ds1)=="panas25")
+
+
+ds1$panas1<- plyr::mapvalues(ds1$panas1, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas2<- plyr::mapvalues(ds1$panas2, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas3<- plyr::mapvalues(ds1$panas3, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas4<- plyr::mapvalues(ds1$panas4, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas5<- plyr::mapvalues(ds1$panas5, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas6<- plyr::mapvalues(ds1$panas6, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas7<- plyr::mapvalues(ds1$panas7, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas8<- plyr::mapvalues(ds1$panas8, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas9<- plyr::mapvalues(ds1$panas9, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas10<- plyr::mapvalues(ds1$panas10, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas11<- plyr::mapvalues(ds1$panas11, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas12<- plyr::mapvalues(ds1$panas12, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas13<- plyr::mapvalues(ds1$panas13, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas14<- plyr::mapvalues(ds1$panas14, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas15<- plyr::mapvalues(ds1$panas15, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas16<- plyr::mapvalues(ds1$panas16, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas17<- plyr::mapvalues(ds1$panas17, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas18<- plyr::mapvalues(ds1$panas18, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas19<- plyr::mapvalues(ds1$panas19, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas20<- plyr::mapvalues(ds1$panas20, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas21<- plyr::mapvalues(ds1$panas21, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas22<- plyr::mapvalues(ds1$panas22, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas23<- plyr::mapvalues(ds1$panas23, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas24<- plyr::mapvalues(ds1$panas24, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+ds1$panas25<- plyr::mapvalues(ds1$panas25, from=c(1,2,3,4,5), to =c(5,4,3,2,1))
+
+#positive affect score c,d,f, g, h, k, p, q, t, u, v, x, y
+#negative affect score a, b, e, i, j, l, m, n, o, r, s, w
+ds2 <- within(ds1, {paffect <- panas3+panas4+panas6+panas7+panas8+panas11+panas17+panas20+panas21+panas22+panas24+panas25
+                    naffect <- panas1+panas2+panas5+panas9+panas10+panas12+panas13+panas14+panas15+panas18+panas19+panas23
+  })
+
+#social effort/reward balance possibility but does not seem to be in the 2014 data set
+
+#Ryff Well-being measure purspose in life scale 06-12 Q35
+setnames(ds2, old=c('lb033a','lb033b','lb033c','lb033d','lb033e','lb033f','lb033g'),
+         new=c('wellb1','wellb2','wellb3', 'wellb4','wellb5','wellb6','wellb7'))
+#Reverse-code items 35 b, d, e, f
+ds2$wellb2<- plyr::mapvalues(ds2$wellb2, from=c(1,2,3, 4, 5, 6), to=c(6,5,4,3,2,1))
+ds2$wellb4<- plyr::mapvalues(ds2$wellb4, from=c(1,2,3, 4, 5, 6), to=c(6,5,4,3,2,1))
+ds2$wellb5<- plyr::mapvalues(ds2$wellb5, from=c(1,2,3, 4, 5, 6), to=c(6,5,4,3,2,1))
+ds2$wellb6<- plyr::mapvalues(ds2$wellb6, from=c(1,2,3, 4, 5, 6), to=c(6,5,4,3,2,1))
+
+s <- which(colnames(ds2)=="wellb1")
+f <- which(colnames(ds2)=="wellb7")
+ds2$wellbtot <-rowSums(ds2[s:f], na.rm=TRUE)
+
+summary(ds2$wellbtot)
+
+m1 <- ifelse(is.na(ds2$wellb1)==TRUE, 1, 0)
+m2 <- ifelse(is.na(ds2$wellb2)==TRUE, 1, 0)
+m3 <- ifelse(is.na(ds2$wellb3)==TRUE, 1, 0)
+m4 <- ifelse(is.na(ds2$wellb4)==TRUE, 1, 0)
+m5 <- ifelse(is.na(ds2$wellb5)==TRUE, 1, 0)
+m6 <- ifelse(is.na(ds2$wellb6)==TRUE, 1, 0)
+m7 <- ifelse(is.na(ds2$wellb7)==TRUE, 1, 0)
+
+ds2$missing<-m1+m2+m3+m4+m5+m6+m7
+ds2$wellbm <- ifelse(ds2$missing<3, ds2$wellbtot/(7-ds2$missing), NA)
+summary(ds2$wellbm)
+
+ds3<- subset(ds2, select=c('lone1','lone2','lone3','lone4','lone5','lone6','lone7',
+                                 'lone8','lone9','lone10','lone11',"lonetot","lonemean",
+                                 'act1','act2','act3','act4','act5','act6','act7','act8',
+                                 'act9','act10','act11','act12','act13','act14'
+                                 ,'act15','act16','act17','act18','act19','act20','act21',
+                                 "mixAct","cogAct","physAct","otherAct", "ActivityTot","lifesatm",
+                                 'snspouse','snchild','snfamily','snfriends','closespouse','closechild','closefam','closefri',
+                                 'closerel','socnetwork','pssspouse','psschild','pssfam','pssfr','nssspouse','nsschild','nssfam','nssfr',
+                                 'paffect','naffect','wellbtot','wellbm'))
 
 # Final step add the year to the  variables
-
-vars<-colnames(ds1)
-for (i in 1:length(ds1)){
+vars<-colnames(ds3)
+for (i in 1:length(ds3)){
   vars[i]<-paste0(vars[i],year_label)
 }
 
