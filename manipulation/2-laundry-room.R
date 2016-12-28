d1e4923a41d615413dbe7ed213fe42e822037188
Andrey Koval <koval.andrey@gmail.com>
2016-12-28 17:31:40 -0500

    kickstarted cleaning scrript

---
+++ manipulation/2-laundry-room.R
@@ -0,0 +1,72 @@
+# knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console 
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # enables piping : %>% 
+library(TabularManifest)
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+# source("./scripts/common-functions.R") # used in multiple reports
+# source("./scripts/graph-presets.R") # fonts, colors, themes 
+# source("./scripts/general-graphs.R") 
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2") # graphing
+# requireNamespace("readr") # data input
+requireNamespace("tidyr") # data manipulation
+requireNamespace("dplyr") # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit")# For asserting conditions meet expected patterns.
+# requireNamespace("car") # For it's `recode()` function.
+
+# ---- declare-globals ---------------------------------------------------------
+
+# ---- load-data ---------------------------------------------------------------
+# load the product of 0-ellis-island.R,  a list object containing data and metadata
+dto <- readRDS("./data-unshared/derived/dto.rds")
+
+# ---- inspect-data -------------------------------------------------------------
+names(dto)
+lapply(dto,names)
+
+
+# ---- tweak-data --------------------------------------------------------------
+
+# ---- basic-table --------------------------------------------------------------
+
+# ---- basic-graph --------------------------------------------------------------
+ds_demo <- dto$demographics
+
+ids <-unique(ds_demo$hhidpn)
+
+
+dto$demographics %>% histogram_discrete("male")
+dto$demographics %>% histogram_continuous("age_at_visit",bin_width = 2)
+dto$demographics %>% histogram_discrete("ses", levels_to_exclude = NA)
+
+
+
+
+
+# ---- publisher ---------------------------------------
+path_report_1 <- "./reports/report.Rmd"
+allReports <- c(
+  path_report_1
+  # ,path_report_2
+  # , ...
+)
+pathFilesToBuild <- c(allReports) ##########
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+# Build the reports
+for( pathFile in pathFilesToBuild ) {
+  
+  rmarkdown::render(input = pathFile,
+                    output_format=c(
+                      "html_document" # set print_format <- "html" in seed-study.R
+                      # "pdf_document"
+                      # ,"md_document"
+                      # "word_document" # set print_format <- "pandoc" in seed-study.R
+                    ),
+                    clean=TRUE)
+}
