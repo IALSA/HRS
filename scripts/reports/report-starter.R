c2172faa49ffda70992946f6e8acdd7e43609a06
Andrey Koval <koval.andrey@gmail.com>
2017-02-21 08:49:48 -0500

    added report scripts from MAP

---
+++ scripts/reports/report-starter.R
@@ -0,0 +1,56 @@
+# knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console 
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # enables piping : %>% 
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+source("./scripts/common-functions.R") # used in multiple reports
+source("./scripts/graph-presets.R") # fonts, colors, themes 
+source("./scripts/general-graphs.R") 
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2") # graphing
+# requireNamespace("readr") # data input
+requireNamespace("tidyr") # data manipulation
+requireNamespace("dplyr") # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit")# For asserting conditions meet expected patterns.
+# requireNamespace("car") # For it's `recode()` function.
+
+# ---- declare-globals ---------------------------------------------------------
+
+# ---- load-data ---------------------------------------------------------------
+# load the product of 0-ellis-island.R,  a list object containing data and metadata
+dto <- readRDS("./data/unshared/derived/dto.rds")
+# each element this list is another list:
+names(dto)
+# 3rd element - data set with unit data
+dplyr::tbl_df(dto[["unitData"]]) 
+# 4th element - dataset with augmented names and labels of the unit data
+dplyr::tbl_df(dto[["metaData"]])
+
+# ---- inspect-data -------------------------------------------------------------
+
+
+# ---- tweak-data --------------------------------------------------------------
+
+# ---- basic-table --------------------------------------------------------------
+
+# ---- basic-graph --------------------------------------------------------------
+# this is how we can interact with the `dto` to call and graph data and metadata
+dto[["metaData"]] %>% 
+  dplyr::filter(type=="demographic") %>% 
+  dplyr::select(name,name_new,label)
+
+dto[["unitData"]]%>%
+  histogram_continuous("age_death", bin_width=1)
+
+dto[["unitData"]]%>%
+  histogram_discrete("msex")
+
+# ---- reproduce ---------------------------------------
+rmarkdown::render(input = "./sandbox/report-a.Rmd" ,
+                  output_format="html_document", clean=TRUE)
