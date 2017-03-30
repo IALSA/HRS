0d4b1a42ca097ad2ab8ca28de17805901bef5b4a
Cassandra <clb@uvic.ca>
2017-03-30 09:52:41 -0700

    replace_na function added

--- sandbox/combine-df.R
+++ sandbox/combine-df.R
@@ -25,7 +25,7 @@ requireNamespace("reshape2")
 
 # ---- load-data ---------------------------------------------------------------
 # load the product of 0-ellis-island.R,  a list object containing data and metadata
-dto <- readRDS("./data-unshared/derived/dto3.rds")
+dto <- readRDS("./data-unshared/derived/dto.rds")
 # each element this list is another list:
 names(dto)
 
@@ -33,6 +33,65 @@ names(dto)
 # merge multiple datasets that are stored as elements of a list
 merge_mulitple_files <- function(list, by_columns){
   Reduce(function( d_1, d_2 ) dplyr::full_join(d_1, d_2, by=by_columns), list)
+}
+
+replace_na <- function(x){
+  #x <- c(NA,1,1)
+  unique_values <- setdiff(unique(x),NA)
+  testit::assert("Error: The value is absent or not unique", length(unique_values)==1L)
+  if(length(unique_values)==1L){
+    x[is.na(x)] <- unique_values
+  }else{
+    x[] <- NA
+  }
+}
+
+# ---- functions-to-examime-temporal-patterns -------------------
+view_temporal_pattern <- function(ds, measure, seed_value = 42){
+  set.seed(seed_value)
+  ds_long <- ds
+  (ids <- sample(unique(ds_long$hhidpn),1))
+  d <-ds_long %>%
+    dplyr::filter(hhidpn %in% ids ) %>%
+    dplyr::select_("hhidpn","year", measure)
+  print(d)
+}
+# ds %>%  view_temporal_pattern("male", 2)
+temporal_pattern <- function(ds, measure){
+  # set.seed(seed_value)
+  ds_long <- ds
+  (ids <- sample(unique(ds_long$hhidpn),1))
+  d <-ds_long %>%
+    dplyr::filter(hhidpn %in% ids ) %>%
+    dplyr::select_("hhidpn","year", measure)
+  print(d)
+}
+
+
+# examine the descriptives over waves
+over_waves <- function(ds, measure_name, exclude_values="") {
+  ds <- as.data.frame(ds)
+  testit::assert("No such measure in the dataset", measure_name %in% unique(names(ds)))
+  # measure_name = "htval"; wave_name = "wave"; exclude_values = c(-99999, -1)
+  cat("Measure : ", measure_name,"\n", sep="")
+  t <- table( ds[,measure_name], ds[,"year"], useNA = "always"); t[t==0] <- ".";t
+  print(t)
+  cat("\n")
+  ds[,measure_name] <- as.numeric(ds[,measure_name])
+  
+  d <- ds[!(ds[,measure_name] %in% exclude_values), ]
+  a <- lazyeval::interp(~ round(mean(var),2) , var = as.name(measure_name))
+  b <- lazyeval::interp(~ round(sd(var),3),   var = as.name(measure_name))
+  c <- lazyeval::interp(~ n())
+  dots <- list(a,b,c)
+  t <- d %>%
+    dplyr::select_("hhidpn","year", measure_name) %>%
+    na.omit() %>%
+    # dplyr::mutate_(measure_name = as.numeric(measure_name)) %>%
+    dplyr::group_by_("year") %>%
+    dplyr::summarize_(.dots = setNames(dots, c("mean","sd","count")))
+  return(as.data.frame(t))
+  
 }
 
 # ---- inspect-data -------------------------------------------------------------
@@ -98,6 +157,58 @@ dto_new[["health"]] <- dto$health %>%
 #View(dto_new$social_support)
 
 ds_long <- merge_mulitple_files(dto_new, by_columns = c("year","hhidpn"))
+
+dplyr::filter(ds_long, hhidpn==3010)
+
+
+# ---- force-to-static-sex ---------------------------
+ds_long %>% view_temporal_pattern("male", 2) # sex
+ds_long %>% over_waves("male") # 1, 2, 3, 4, 5, 6
+# check that values are the same across waves
+ds_long %>%
+  dplyr::group_by(hhidpn) %>%
+  dplyr::summarize(unique = length(unique(male[!is.na(male)]))) %>%
+  dplyr::arrange(desc(unique)) # unique > 1 indicates change over wave
+
+# grab the value for the first wave and forces it to all waves 
+ds_long <- ds_long %>%
+  dplyr::group_by(hhidpn) %>%
+  dplyr::mutate(
+    male = dplyr::first(male) # grabs the value for the first wave and forces it to all waves
+  ) %>%
+  dplyr::ungroup()
+# examine the difference
+ds_long %>% over_waves("male")
+ds_long %>% view_temporal_pattern("male", 2) # sex
+
+
+
+
+ds <- readRDS("./data-unshared/derived/data-long.rds")
+
+d_long <- ds %>% 
+  dplyr::filter(hhidpn == 3020) %>%
+  dplyr::select(hhidpn, year, male.x, race_rand, exercise, srmemory)
+
+# ---- a function that checks for NA values in demographic variables that should be consistent -----
+print(d_long)
+patch_na <- function(
+  d, # input dataset in long format with respect to time
+  varname # name of the variable the values of which to patch
+){
+  # test whether variable has a single unique value or NA
+  # replace all NA's values with that unique value
+  d <- d_long
+  d1 <- d %>%
+    dplyr::group_by(hhidpn) %>%
+    dplyr::mutate(
+      unique_value = ifelse(unique(varname)
+    )
+    
+}
+# usage:
+ d_long <- d_long %>% patch_na("male")
+print(d_long)
 
 
 
@@ -264,20 +375,34 @@ ds_long2%>%
   dplyr::summarize(unique = length(unique(raedegrm[!is.na(raedegrm)]))) %>%
   dplyr::arrange(desc(unique)) 
 
-# ---- prepare-for-mplus ---------------------
+ds_long)
+
+
+
+
+d_long <- ds_long %>% 
+  dplyr::filter(hhidpn == 3010) %>%
+  dplyr::select(hhidpn, year, male, exercise, srmemory)
+d_long
+
 # define variable properties for long-to-wide conversion
-variables_static <- c("hhidpn", "male", "birthyr_rand", "birthmo_rand",
-                      "race_rand", "hispanic_rand", "cohort", "raedyrs", "raedegrm")
-
-variables_longitudinal <- setdiff(colnames(ds_long2),variables_static)  # not static
+variables_static <- c("hhidpn", "male", "birthyr_rand", "birthmo_rand", "race_rand", "hispanic_rand", "cohort", "raedyrs","raedegrm")
+variables_longitudinal <- setdiff(colnames(ds_long),variables_static)  # not static
 (variables_longitudinal <- variables_longitudinal[!variables_longitudinal=="year"]) # all except year
-# establish a wide format
-#variables_longitudinal <- variables_longitudinal[1:5]
-#variables_static <- variables_static[1]
 
-dplyr::glimpse(ds_long2)
-
-ds_wide <- ds_long2 %>%
+d_wide <- d_long %>%
+  dplyr::select_(.dots = c(variables_static,  "year", variables_longitudinal))  %>%
+  tidyr::gather_(key="variable", value="value", variables_longitudinal)  %>%
+  dplyr::mutate(year=as.character(year)) %>%
+  dplyr::mutate(male=as.character(male)) %>%
+  dplyr::arrange(hhidpn) %>% 
+  dplyr::mutate(
+    # variable = gsub("^v","",variable),
+    temp = paste0(variable,"_",year)) %>%
+  dplyr::select(-variable,-year) %>% 
+  tidyr::spread(temp, value)
+  
+ds_wide <- ds_long %>%
   dplyr::select_(.dots = c(variables_static,  "year", variables_longitudinal))  %>%
   tidyr::gather_(key="variable", value="value", variables_longitudinal)  %>%
   # dplyr::mutate(wave = as.character(seq_along(unique(year)))) %>%
@@ -287,8 +412,8 @@ ds_wide <- ds_long2 %>%
   dplyr::arrange(hhidpn) %>% 
   dplyr::mutate(
     # variable = gsub("^v","",variable),
-    temp = paste0(variable,"_",wave)) %>%
-  dplyr::select(-variable,-year,-wave) %>% 
+    temp = paste0(variable,"_",year)) %>%
+  dplyr::select(-variable,-year) %>% 
   tidyr::spread(temp, value)
 ds_wide
 
