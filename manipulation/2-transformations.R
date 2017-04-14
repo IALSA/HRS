e29482a6edb83670f9282a4c7f5683c89b6da0b9
Cassandra <clb@uvic.ca>
2017-04-14 12:37:51 -0700

    Transformations script created

---
+++ manipulation/2-transformations.R
@@ -0,0 +1,26 @@
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console
+
+# ---- load-sources ------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  
+# Ideally, no real operations are performed in these sourced scripts. 
+source("./scripts/functions-common.R") # used in multiple reports
+
+# ---- load-packages ----------------------------------------------
+# Attach packages so their functions don't need to be qualified when used
+# See more : http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # Pipes
+library(ggplot2) # Graphs
+# Functions of these packages will need to be qualified when used
+# See more: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("tidyr") #  data manipulation
+requireNamespace("dplyr") # f() names conflict with other packages (esp. base, stats, and plyr).
+requireNamespace("testit") # for asserting conditions meet expected patterns.
+
+# ---- load-data ------------------------------------------------
+# ---- inspect-data ------------------------------------------------
+# ---- basic-table ------------------------------------------------
+# ---- basic-graph ------------------------------------------------
+# ----  ------------------------------------------------
+# ----  ------------------------------------------------
+# ----  ------------------------------------------------
