938dfeffee7120ee4c0416483d8e08d2bae2b588
Cassandra <clb@uvic.ca>
2017-04-20 08:02:58 -0700

    Moved demographic transformations

--- manipulation/2-transformations.R
+++ manipulation/2-transformations.R
@@ -18,6 +18,7 @@ library(dplyr)
 requireNamespace("tidyr") #  data manipulation
 requireNamespace("dplyr") # f() names conflict with other packages (esp. base, stats, and plyr).
 requireNamespace("testit") # for asserting conditions meet expected patterns.
+requireNamespace("zoo")
 # ---- load-globals ------------------------------------------------
 path_dto_input <- "./data-unshared/derived/dto_raw.rds"
 path_dto_output <- "./data-unshared/derived/dto.rds"
@@ -54,45 +55,6 @@ dto_new %>%
 # remove unnecessary columns
 dto_new <- dto_new %>% 
   dplyr::select(-serial7r_tot, -serial7r_tot_2014)
-
-# Two chunks below are taken from 
-# https://github.com/IALSA/MAP/blob/master/manipulation/1-transformations.R#L85
-# ---- temporal-traingulation --------------------
-ds <- ds %>% 
-  dplyr::mutate(
-    age_in_days_visit = round(age_at_visit * 365, 0),
-    age_in_days_bl    = round(age_at_bl * 365, 0),
-    birth_date        = date_at_bl - age_in_days_bl, 
-    birth_year        = lubridate::year(birth_date), 
-    date_at_visit     = birth_date + age_in_days_visit 
-  ) %>% 
-  dplyr::select(-age_in_days_visit, - age_in_days_bl)
-
-ds %>% 
-  dplyr::select(id,birth_date,birth_year,date_at_bl,age_at_bl, fu_year, date_at_visit, age_at_visit) %>%
-  dplyr::slice(1:10) %>% print() 
-
-
-# names_labels(ds) %>% head()
-# ---- compute-death-indicator --------------------
-ds <- ds %>% 
-  dplyr::group_by(id) %>%  
-  dplyr::mutate(   
-    died           = ifelse(any(!is.na(age_death)),1,0)
-  ) %>% 
-  dplyr::ungroup()
-# names_labels(ds) %>% head()
-
-# inspect transformation
-set.seed(49)
-ids <- sample(unique(ds$id), 3)
-ids <- 33027
-ds %>% 
-  dplyr::filter(id %in% ids) %>% 
-  dplyr::select(id, birth_date,age_at_bl,fu_year,age_at_visit,age_death, died  ) %>% 
-  print()
-
-
 
 # ---- create-leave-behind-indicator -----------------------------
 leave_behind_measures <- c(
@@ -135,12 +97,12 @@ dto_new %>%
   dplyr::filter(id == "3020") %>%
   dplyr::select_(.dots = c("id","year",leave_behind_measures)) %>%
   dplyr::mutate(
-    # add_valid_values = rowSums(.[leave_behind_measures],na.rm = T),
+    add_valid_values = rowSums(.[leave_behind_measures],na.rm = T),
     # add_valid_values = sum(is.na(.[leave_behind_measures])),  
     
     leave_behind_flag = ifelse(add_valid_values , TRUE, FALSE )
   ) %>%
-  as.data.frame()
+  #as.data.frame()
 
 
   dplyr::group_by(id, leave_behind_flag) %>%
@@ -156,7 +118,7 @@ dto_new %>%
 # implement for the entire data set
 dto <- dto_new %>% 
   # dplyr::filter(id == "1010") %>%
-  dplyr::select_(.dots = c("id","year",leave_behind_measures)) %>%
+  #dplyr::select_(.dots = c("id","year",leave_behind_measures)) %>%
   dplyr::mutate(
     add_valid_values = rowSums(.[leave_behind_measures],na.rm = T),
     leave_behind_flag = ifelse(add_valid_values , TRUE, FALSE )
@@ -182,6 +144,8 @@ dto <- dto_new %>%
 
 # DEVELOPMENTAL SCRIPT AFTER THIS LINE
 
+ds_long <- dto_raw
+
 # A list of the psychosocial variables to use to check for completion of the psychosocial variables.
 ds_lbvars <- ds_long %>% 
   dplyr::select(score_loneliness_3, score_loneliness_11, snspouse, snchild, 
@@ -202,5 +166,88 @@ ds_long <- ds_long %>%
 # create an indicator only of waves  
 ds_long$lbwave <- ifelse(ds_long$lbqs==1, ds_long$lbwavecount, 0)
 
+# designate the first lbwave as baseline
+
+ds_long <- ds_long %>%
+  #dplyr::group_by(hhidpn)
+  dplyr::mutate(
+    interview_date = paste0(interview_mth,"/",interview_yr),
+    interview_date = zoo::as.yearmon(interview_date, "%m/%Y"),
+    hrs_tscore = interview_date-dplyr::lag(interview_date)
+  )
+
+ds_long %>% 
+  dplyr::select(hhidpn, year, hrs_tscore, starts_with("interview_")) %>% 
+  dplyr::filter(hhidpn == "3020")
+  
+test <- ds_long %>% 
+  #dplyr::group_by(hhidpn) %>% 
+  dplyr::group_by(hhidpn) %>%
+  dplyr::mutate (
+    row = ds_long[,"lbwave"]==2
+  ) %>% 
+  dplyr::ungroup()
+
+
+which(ds_long[,""])     
+ds_long$lb_tscore = ifelse(ds_long[,lbwave]==1, 0,
+                   ifelse(ds_long[,lbwave]>1, interview_date-dplyr::lag(interview_date),NA))
+
+ds_long$lbtscore <- ifelse(ds_long$lbwave==1, 0, 
+                   ifelse(ds_long$lbwave==2, ds_long$intdate_rand))
+ds_long[ds_long[,"lbwave"]==2 & ds_long[,"hhidpn"]==
+
+head(ds_long$intdate_rand)
+
+ds_long %>% 
+  dplyr::select(hhidpn, year, starts_with("interview_")) %>% 
+  dplyr::filter(hhidpn == "3010") %>% 
+  dplyr::mutate(
+    lb_tscore = ifelse(ds_long[,lbwave]==1, 0,
+                       ifelse(ds_long[,lbwave]>1, interview_date-dplyr::lag(interview_date),NA))
+  )
+
+
 # subset the data frame to include only those belonging to a cohort.
 ds_long <- subset(ds_long, cohort!= 0)
+
+
+# Two chunks below are taken from 
+# https://github.com/IALSA/MAP/blob/master/manipulation/1-transformations.R#L85
+# ---- temporal-traingulation --------------------
+ds <- ds %>% 
+  dplyr::mutate(
+    age_in_days_visit = round(age_at_visit * 365, 0),
+    age_in_days_bl    = round(age_at_bl * 365, 0),
+    birth_date        = date_at_bl - age_in_days_bl, 
+    birth_year        = lubridate::year(birth_date), 
+    date_at_visit     = birth_date + age_in_days_visit 
+  ) %>% 
+  dplyr::select(-age_in_days_visit, - age_in_days_bl)
+
+ds %>% 
+  dplyr::select(id,birth_date,birth_year,date_at_bl,age_at_bl, fu_year, date_at_visit, age_at_visit) %>%
+  dplyr::slice(1:10) %>% print() 
+
+
+# names_labels(ds) %>% head()
+# ---- compute-death-indicator --------------------
+ds <- ds %>% 
+  dplyr::group_by(id) %>%  
+  dplyr::mutate(   
+    died           = ifelse(any(!is.na(age_death)),1,0)
+  ) %>% 
+  dplyr::ungroup()
+# names_labels(ds) %>% head()
+
+# inspect transformation
+set.seed(49)
+ids <- sample(unique(ds$id), 3)
+ids <- 33027
+ds %>% 
+  dplyr::filter(id %in% ids) %>% 
+  dplyr::select(id, birth_date,age_at_bl,fu_year,age_at_visit,age_death, died  ) %>% 
+  print()
+
+
+
